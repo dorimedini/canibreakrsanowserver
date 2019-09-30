0b3ff8915d2ec3d1fe30054f2036366c13ec65b8
dorimedini <dorimedini@gmail.com>
2019-09-30 18:15:26 +0300

    Now respond with QISKit code
    Signed-off-by: dorimedini <dorimedini@gmail.com>

---
+++ Q.py
@@ -0,0 +1,50 @@
+from qiskit import *
+from qiskit.providers import JobStatus
+from qiskit.tools.visualization import plot_histogram, plot_bloch_multivector
+from qiskit.tools.monitor import job_monitor
+from time import sleep
+
+
+class Q:
+    @staticmethod
+    def _is_job_done(job):
+        return job.status() in [JobStatus.CANCELLED, JobStatus.DONE, JobStatus.ERROR]
+
+    @staticmethod
+    def _job_state_response(job):
+        status = job.status()
+        if status == JobStatus.QUEUED:
+            return "In queue ({})".format(job.queue_position())
+        if status == JobStatus.INITIALIZING:
+            return "Initializing job..."
+        if status == JobStatus.RUNNING:
+            return "Running job..."
+        if status == JobStatus.VALIDATING:
+            return "Validating job..."
+        return "ERROR: Unhandled status '{}'".format(status.name)
+
+    @staticmethod
+    def _job_end_response(job, circuit):
+        status = job.status()
+        msg = "Job ended with message:\n{}".format(status.value)
+        if status == JobStatus.DONE:
+            msg += "\nResult histogram data:\n{}".format(job.result().get_counts(circuit))
+        return msg
+
+    @staticmethod
+    def generate_test_quantum_run_response(interval=5):
+        qr = QuantumRegister(2)
+        cr = ClassicalRegister(2)
+        circuit = QuantumCircuit(qr, cr)
+        circuit.h(qr[0])
+        circuit.cx(qr[0], qr[1])
+        circuit.measure(qr, cr)
+        IBMQ.load_account()
+        provider = IBMQ.get_provider('ibm-q')
+        qcomp = provider.get_backend('ibmq_ourense')
+        job = execute(circuit, backend=qcomp)
+        while not Q._is_job_done(job):
+            yield Q._job_state_response(job)
+            sleep(interval)
+        return Q._job_end_response(job, circuit)
+
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
+from Q import Q
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Server Works!'
+    return Response(Q.generate_test_quantum_run_response())
 
 
 @app.route('/greet')
