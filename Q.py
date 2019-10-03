7e11cab711b57a504e26627e7031952053d04003
dorimedini <dorimedini@gmail.com>
2019-10-03 21:25:20 +0300

    Move to busy-query client communication
    The client now periodically sends requests until done. The Q class now
    stores a static state (keyed by request address for now) which is
    updated on a separate thread while the request immediately returns a
    response
    
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- Q.py
+++ Q.py
@@ -1,11 +1,19 @@
-from qiskit import *
+from qiskit import execute
+from qiskit.circuit import ClassicalRegister, QuantumRegister, QuantumCircuit
 from qiskit.providers import JobStatus
+from qiskit.providers.ibmq import IBMQ
 from qiskit.tools.visualization import plot_histogram, plot_bloch_multivector
 from qiskit.tools.monitor import job_monitor
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from flask import request
 from time import sleep
 
 
 class Q:
+
+    _RESPONSE_MAP = {}
+    _CIRCUIT_MAP = {}
+
     @staticmethod
     def _is_job_done(job):
         return job.status() in [JobStatus.CANCELLED, JobStatus.DONE, JobStatus.ERROR]
@@ -24,26 +32,41 @@ class Q:
         return "ERROR: Unhandled status '{}'".format(status.name)
 
     @staticmethod
-    def _job_end_response(job, circuit):
+    def _job_end_response(job, circ):
         status = job.status()
         msg = "Job ended with message:\n{}".format(status.value)
         if status == JobStatus.DONE:
-            msg += "\nResult histogram data:\n{}".format(job.result().get_counts(circuit))
+            msg += "\nResult histogram data:\n{}".format(job.result().get_counts(circ))
         return msg
 
     @staticmethod
+    def execute_circuit(key, circ, provider_name, backend_name, interval=5.0):
+        IBMQ.load_account()
+        provider = IBMQ.get_provider(provider_name)
+        qcomp = provider.get_backend(backend_name)
+        job = execute(circ, backend=qcomp)
+        while not Q._is_job_done(job):
+            Q._RESPONSE_MAP[key] = Q._job_state_response(job)
+            sleep(interval)
+        Q._RESPONSE_MAP[key] = Q._job_end_response(job, circ)
+
+    @staticmethod
     def test_quantum_run_response():
+        key = request.remote_addr
+        if key in Q._RESPONSE_MAP:
+            return Q._RESPONSE_MAP[key]
+        Q._RESPONSE_MAP[key] = "Constructing circuit..."
         qr = QuantumRegister(2)
         cr = ClassicalRegister(2)
-        circuit = QuantumCircuit(qr, cr)
-        circuit.h(qr[0])
-        circuit.cx(qr[0], qr[1])
-        circuit.measure(qr, cr)
-        IBMQ.load_account()
-        provider = IBMQ.get_provider('ibm-q')
-        qcomp = provider.get_backend('ibmq_ourense')
-        job = execute(circuit, backend=qcomp)
-        while not Q._is_job_done(job):
-            yield Q._job_state_response(job)
-        return Q._job_end_response(job, circuit)
-
+        circ = QuantumCircuit(qr, cr)
+        circ.h(qr[0])
+        circ.cx(qr[0], qr[1])
+        circ.measure(qr, cr)
+        Q._CIRCUIT_MAP[key] = circ
+        Q._RESPONSE_MAP[key] = "Connecting to backend..."
+        scheduler = AsyncIOScheduler()
+        scheduler.add_job(Q.execute_circuit,
+                          args=[key, circ, 'ibm-q', 'ibmq_ourense'],
+                          trigger='date')
+        scheduler.start()
+        return Q._CIRCUIT_MAP[key]
