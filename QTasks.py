8455622129922ca73c415af307a3c93b0923466a
dorimedini <dorimedini@gmail.com>
2019-10-14 14:16:38 +0300

    Moved all locking to decorators
    This includes debug prints so it'll be easier to identify deadlocks if
    they occur
    
    Signed-off-by: dorimedini <dorimedini@gmail.com>

--- QTasks.py
+++ QTasks.py
@@ -13,6 +13,30 @@ from verbose import Verbose
 import concurrent.futures
 
 
+def read_locked(f):
+    def lock_read_run(self, *args, **kwargs):
+        self.logger.debug("Read-locking for method {}".format(f.__name__))
+        self._rw_lock.acquire_read()
+        self.logger.debug("Read-locked for method {}".format(f.__name__))
+        ret = f(self, *args, **kwargs)
+        self._rw_lock.release_read()
+        self.logger.debug("Read-unlocked for method {}".format(f.__name__))
+        return ret
+    return lock_read_run
+
+
+def write_locked(f):
+    def lock_write_run(self, *args, **kwargs):
+        self.logger.debug("Write-locking for method {}".format(f.__name__))
+        self._rw_lock.acquire_write()
+        self.logger.debug("Write-locked for method {}".format(f.__name__))
+        ret = f(self, *args, **kwargs)
+        self._rw_lock.release_write()
+        self.logger.debug("Write-unlocked for method {}".format(f.__name__))
+        return ret
+    return lock_write_run
+
+
 class QNoBackendException(Exception):
     pass
 
@@ -53,16 +77,10 @@ class _QTasks(Verbose):
         self._abort.set()
 
     def request_job(self, job_descriptor: ShorJobDescriptor):
-        key = str(job_descriptor)
-        self._rw_lock.acquire_read()
-        if key in self._job_states:
-            state = self._job_states[key]
-            self._rw_lock.release_read()
+        state = self._get_state(job_descriptor)
+        if state:
             return str(state)
-        self._rw_lock.release_read()
-        self._rw_lock.acquire_write()
         self._submit_job(job_descriptor)
-        self._rw_lock.release_write()
         return str(self._get_state(job_descriptor))
 
     def request_cancel(self, job_descriptor: ShorJobDescriptor):
@@ -72,9 +90,7 @@ class _QTasks(Verbose):
             return str(QResponse(key,
                                  status=QStatus.JOB_NOT_FOUND,
                                  server_response="Job not found. nothing to cancel"))
-        self._rw_lock.acquire_write()
-        self._cancellation_events[key].set()
-        self._rw_lock.release_write()
+        self._fire_cancel_event(key)
         return str(QResponse(key,
                              status=QStatus.CANCELLED,
                              server_response="Cancelling job"))
@@ -86,22 +102,47 @@ class _QTasks(Verbose):
             return str(QResponse(key, status=QStatus.JOB_NOT_FOUND))
         return str(state)
 
+    @write_locked
+    def _fire_cancel_event(self, key):
+        self._cancellation_events[key].set()
+
+    @write_locked
     def _submit_job(self, job_descriptor: ShorJobDescriptor):
         k = str(job_descriptor)
-        self._rw_lock.acquire_write()
         self._cancellation_events[k] = Event()
         self._job_states[k] = QResponse(key=k, status=QStatus.REQUESTED)
         self._job_queue.put(job_descriptor)
-        self._rw_lock.release_write()
 
+    @read_locked
     def _get_state(self, job_descriptor: ShorJobDescriptor) -> QResponse:
         key = str(job_descriptor)
         state = None
-        self._rw_lock.acquire_read()
         if key in self._job_states:
             state = self._job_states[key]
-        self._rw_lock.release_read()
         return state
+
+    @read_locked
+    def _get_cancellatioin_event(self, key):
+        return self._cancellation_events[key]
+
+    @write_locked
+    def _delete_cancellation_event(self, key):
+        del self._cancellation_events[key]
+
+    @write_locked
+    def _delete_job_state(self, key):
+        del self._job_states[key]
+
+    @write_locked
+    def _update_status_internal(self, key, status, queue_pos, results, error):
+        self._job_states[key].status = status
+        self._job_states[key].update_response_from_status()
+        if status == QStatus.QUEUED:
+            self._job_states[key].queue_position = int(queue_pos)
+        elif status == QStatus.ERROR:
+            self._job_states[key].error = error
+        elif status == QStatus.DONE:
+            self._job_states[key].result = results
 
     def _update_status(self,
                        job_descriptor: ShorJobDescriptor,
@@ -109,7 +150,7 @@ class _QTasks(Verbose):
                        place_in_queue=-1,
                        results=None,
                        error=None):
-        if status == QStatus.QUEUED and place_in_queue < 0:
+        if status == QStatus.QUEUED and int(place_in_queue) < 0:
             raise ValueError("Job '{}' updated to QUEUED state but no place in queue provided"
                              "".format(str(job_descriptor)))
         elif status == QStatus.ERROR and not error:
@@ -118,16 +159,7 @@ class _QTasks(Verbose):
         key = str(job_descriptor)
         self.logger.debug("In update_status for job {}. status: {}, queue: {}, error: {}, results: {}"
                           "".format(key, status, place_in_queue, error, results))
-        self._rw_lock.acquire_write()
-        self._job_states[key].status = status
-        self._job_states[key].update_response_from_status()
-        if status == QStatus.QUEUED:
-            self._job_states[key].queue_position = int(place_in_queue)
-        elif status == QStatus.ERROR:
-            self._job_states[key].error = error
-        elif status == QStatus.DONE:
-            self._job_states[key].result = results
-        self._rw_lock.release_write()
+        self._update_status_internal(key, status, place_in_queue, results, error)
 
     def _update_status_by_job(self, job_descriptor: ShorJobDescriptor, job, circ):
         status = from_job_status(job.status())
@@ -185,9 +217,7 @@ class _QTasks(Verbose):
     def _do_job(self, job_descriptor: ShorJobDescriptor):
         key = str(job_descriptor)
         self.logger.debug("In do_job for job {}".format(key))
-        self._rw_lock.acquire_read()
-        cancellation_event = self._cancellation_events[key]
-        self._rw_lock.release_read()
+        cancellation_event = self._get_cancellatioin_event(key)
         job, circ = self._start_shor_job(job_descriptor)
         if not job:
             self.logger.debug("Job {} failed to start (backend error)".format(key))
@@ -240,16 +270,12 @@ class _QTasks(Verbose):
                             'job': key,
                             'cleanup_time': QTasks.CLEANUP_TIMEOUT + int(round(time()))
                         })
-                        self._rw_lock.acquire_write()
                         del futures[future]
-                        del self._cancellation_events[key]
-                        self._rw_lock.release_write()
+                        self._delete_cancellation_event(key)
                         done_futures += 1
                 while pending_cleanup and pending_cleanup[0]['cleanup_time'] <= int(round(time())):
                     key = pending_cleanup[0]['job']
-                    self._rw_lock.acquire_write()
-                    del self._job_states[key]
-                    self._rw_lock.release_write()
+                    self._delete_job_state(key)
                     pending_cleanup.pop(0)
                     cleaned += 1
                 if iteration == log_every_n_iterations:
